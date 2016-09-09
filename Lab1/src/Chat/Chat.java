a1f1bb46dfa7fe1bd256ad881207f848155a518f
Steenson <lilannie@iastate.edu>
2016-09-09 14:40:05 -0500

    Setting up main class

---
+++ Lab1/src/Chat/Chat.java
@@ -0,0 +1,16 @@
+package Chat;
+
+import java.util.Scanner;
+
+public class Chat {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your name: (Type in your name, then press Enter");
+		String name = scan.next();
+		
+		scan.close();
+
+	}
+
+}
