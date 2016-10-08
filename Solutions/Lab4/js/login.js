6abeebe57f1307c977e93f39cbbdb089586090af
Figgynewts <znewton@iastate.edu>
2016-10-08 17:48:10 -0500

    Added login

--- Solutions/Lab4/js/login.js
+++ Solutions/Lab4/js/login.js
@@ -2,4 +2,15 @@ function attemptLogin(){
     var username = $('#usernameInput').val();
     var password = $('#passwordInput').val();
     console.log(username + ': ' + password);
+    if (username == 'admin' && password == 'admin')
+    {
+        console.log('Logged in as Librarian');
+    }
+    else if (username.substr(0,1) == 'U')
+    {
+        console.log('Logged in as Undregraduate Student');
+    } else {
+        alert('Not a correct username and password');
+    }
 }
+
