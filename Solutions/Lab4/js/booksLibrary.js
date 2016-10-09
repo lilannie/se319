02c1e16cb28ad1deea1ad9a08de5b31f43c16981
Figgynewts <znewton@iastate.edu>
2016-10-08 20:53:55 -0500

    Student mostly functional, just gotta check for only 2 borrows

--- Solutions/Lab4/js/booksLibrary.js
+++ Solutions/Lab4/js/booksLibrary.js
@@ -20,6 +20,7 @@ function Library(){
     }
 
     this.currentAuthorization = 'none';
+    this.currentUser = '';
 
     this.begin();
 }
@@ -31,30 +32,39 @@ Library.prototype.attemptLogin = function(){
     {
         console.log('Logged in as Librarian');
         this.currentAuthorization = 'librarian';
+        this.currentUser = username;
     }
     else if (username.substr(0,1) == 'U')
     {
         console.log('Logged in as Undregraduate Student');
         this.currentAuthorization = 'student';
+        this.currentUser = username;
     } else {
         alert('Not a correct username and password');
     }
 };
 Library.prototype.render = function(){
-    var html = '<table><tbody>';
+    var table = document.createElement('table');
     for(var s = 0; s < this.shelves.length; s++){
         var shelf = this.shelves[s];
-        html += '<tr class="shelf"><th>'+shelf.name+'</th>';
+        var row = document.createElement('tr');
+        row.className = 'shelf';
+        var header = document.createElement('th');
+        header.innerHTML = shelf.name;
+        row.appendChild(header);
+        // html += '<tr class="shelf"><th>'+shelf.name+'</th>';
         for(var b = 0; b < shelf.books.length; b++){
             var book = shelf.books[b];
-            html += '<td id="'+book.type+book.id+'" class="book '+ (book.presence ? '' : 'borrowed') +'">';
-            html += book.type + book.id;
-            html += '</td>';
+            var cell = document.createElement('td');
+            cell.className = 'book ' + (book.presence ? '' : 'borrowed');
+            cell.id = book.type+'_'+book.id;
+            cell.onclick = book.clickFunction(this.currentAuthorization, this.currentUser);
+            cell.innerHTML = book.type + book.id;
+            row.appendChild(cell);
         }
-        html += '</tr>';
+        table.appendChild(row);
     }
-    html += '</tbody></table>';
-    $('#view').html(html);
+    document.getElementById('view').appendChild(table);
 };
 Library.prototype.begin = function(){
     this.attemptLogin();
@@ -72,14 +82,68 @@ function Book(type, id){
     this.presence = 1;
 }
 Book.prototype.borrow = function(borrower){
-    if(this.type != 'R' && this.presence == 1){
-        this.borrowedBy = borrower;
-        this.presence = 0;
+    var that = this;
+    return function() {
+        if (that.type != 'R' && that.presence) {
+            console.log(borrower + ' borrowed ' + that.type + that.id);
+            document.getElementById(that.type + '_' + that.id).classList.add('borrowed');
+            that.borrowedBy = borrower;
+            that.presence = 0;
+            $('#info').hide();
+            $('#info-content').html('');
+        }
     }
 };
 Book.prototype.return = function(){
-    this.borrowedBy = '';
-    this.presence = 1;
+    var that = this;
+    return function() {
+        console.log(that.type + that.id + ' returned');
+        document.getElementById(that.type + '_' + that.id).classList.remove('borrowed');
+        that.borrowedBy = '';
+        that.presence = 1;
+        $('#info').hide();
+        $('#info-content').html('');
+    }
+};
+Book.prototype.clickFunction = function(auth, user){
+    var that = this;
+    return function(){
+        if(auth == 'librarian')
+        {
+            var bookInfo = [
+                'Type: ' + that.type,
+                '<br>',
+                'ID: ' + that.id,
+                '<br>',
+                'Borrowed By: ' + (!that.presence ? 'No One' : that.borrowedBy),
+                '<br>'
+            ];
+            $('info-content').html(bookInfo.join(''));
+        }
+        else if (auth == 'student' && that.type != 'R')
+        {
+            if(that.presence)
+            {
+                var borrowButton = document.createElement('button');
+                borrowButton.disabled = !that.presence;
+                borrowButton.onclick = that.borrow(user);
+                borrowButton.innerHTML = 'Borrow '+that.type+that.id;
+                // var borrowHtml = '<button '+(that.presence ? '' : 'disabled')+' onclick="'+that.borrow(user)+'">Borrow'+that.type+that.id+'</button>';
+                $('#info-content').append(borrowButton);
+                $('#info').show();
+            }
+            else
+            {
+                var returnButton = document.createElement('button');
+                returnButton.disabled = that.presence || that.borrowedBy != user;
+                returnButton.onclick = that.return();
+                returnButton.innerHTML = 'Return '+that.type+that.id;
+                // var returnHtml = '<button disabled="'+((!that.presence && that.borrowedBy == user) ? 'false' : 'true')+'" onclick="'+that.return()+'">Return</button>';
+                $('#info-content').append(returnButton);
+                $('#info').show();
+            }
+        }
+    }
 };
 
 function Shelf(name, category){
