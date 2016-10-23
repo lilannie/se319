158aa29fae3664c33c3e861f4d59e199d77517ca
Annie Steenson <anniesteenso@gmail.com>
2016-10-23 15:21:08 -0500

    Finished part of Lab2

--- Solutions/Lab2/public/js/validation2.js
+++ Solutions/Lab2/public/js/validation2.js
@@ -1,3 +1,34 @@
-/**
- * Created by lilannaye on 9/14/16.
- */
+$(document).ready(function () {
+    $("#success").hide();
+    $("#failure").hide();
+});
+
+function checkForm() {
+    if ($("#complete").html() == 'Finished') {
+        window.location = "./googleMap.html";
+    }
+    var emailFlag = false, phoneNumberFlag = false, addressFlag = false;
+    if (isValidEmail(document.forms['contact']['email'].value)) {
+        $("#emailCheck").removeClass('fa-check-square-o');
+        $("#emailCheck").addClass('fa-check-square');
+        emailFlag = true;
+    }
+    if (isValidPhoneNumber(document.forms['contact']['phoneNumber'].value)) {
+        $("#phoneNumberCheck").removeClass('fa-check-square-o');
+        $("#phoneNumberCheck").addClass('fa-check-square');
+        phoneNumberFlag = true;
+    }
+    if (isValidAddress(document.forms['contact']['address'].value)) {
+        $("#addressCheck").removeClass('fa-check-square-o');
+        $("#addressCheck").addClass('fa-check-square');
+        addressFlag = true;
+        localStorage.setItem("address", document.forms['contact']['address'].value);
+    }
+    if (emailFlag && phoneNumberFlag && addressFlag) {
+        $("#success").slideDown();
+        $('#email').prop('disabled', true);
+        $('#phoneNumber').prop('disabled', true);
+        $('#address').prop('disabled', true);
+        $("#complete").html('Finished');
+    }
+}
