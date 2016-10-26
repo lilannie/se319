7f3dd926755fd2f3caf186469737d2c2800ef66d
Figgynewts <znewton@iastate.edu>
2016-10-26 16:08:25 -0500

    Trying to make basic layout components

--- Solutions/DartPortfolio/lib/app_component.dart
+++ Solutions/DartPortfolio/lib/app_component.dart
@@ -1,5 +1,18 @@
 import 'package:angular2/core.dart';
+import 'package:angular2_quickstart/navbar_component.dart';
+import 'package:angular2_quickstart/drawer_component.dart';
+import 'package:angular2_quickstart/content_component.dart';
+import 'package:angular2_quickstart/footer_component.dart';
 @Component(
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>')
+    template: '''
+    <navbar></navbar>
+
+    <drawer></drawer>
+
+    <content></content>
+
+    <custom-footer></custom-footer>
+    ''',
+    directives: const [NavbarComponent,DrawerComponent,ContentComponent,FooterComponent])
 class AppComponent {}
