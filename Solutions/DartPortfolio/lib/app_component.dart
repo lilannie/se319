7097b50dae27f504af28098e2df7fd6fc6f18d68
Figgynewts <znewton@iastate.edu>
2016-10-28 14:50:16 -0500

    Routing kinda

--- Solutions/DartPortfolio/lib/app_component.dart
+++ Solutions/DartPortfolio/lib/app_component.dart
@@ -1,10 +1,16 @@
 import 'package:angular2/core.dart';
-import 'package:angular2_quickstart/components/navbar_component.dart';
-import 'package:angular2_quickstart/components/drawer_component.dart';
-import 'package:angular2_quickstart/components/content_component.dart';
-import 'package:angular2_quickstart/components/footer_component.dart';
+import 'package:angular2/router.dart';
+
+//import 'hero_service.dart';
+import 'package:dart_portfolio/components/test_component.dart';
+import 'package:dart_portfolio/components/drawer_component.dart';
+import 'package:dart_portfolio/components/footer_component.dart';
+import 'package:dart_portfolio/components/navbar_component.dart';
+@RouteConfig(const [
+  const Route(path: '/', name: 'Home', component: TestComponent)
+])
 @Component(
-    selector: 'my-app',
+    selector: 'app',
     template: '''
 <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
   <app-navbar class="android-header mdl-layout__header mdl-layout__header--waterfall"></app-navbar>
@@ -12,11 +18,13 @@ import 'package:angular2_quickstart/components/footer_component.dart';
   <app-drawer class="android-drawer mdl-layout__drawer"></app-drawer>
 
   <div class="android-content mdl-layout__content">
-    <app-content></app-content>
+    <router-outlet></router-outlet>
 
     <app-footer class="android-footer mdl-mega-footer"></app-footer>
   </div>
 </div>
-''',
-    directives: const [NavbarComponent,DrawerComponent,ContentComponent,FooterComponent])
-class AppComponent {}
+      ''',
+    directives: const [ROUTER_DIRECTIVES,DrawerComponent,FooterComponent,NavbarComponent],
+    providers: const [ROUTER_PROVIDERS])
+class AppComponent {
+}
--- Solutions/DartPortfolio/lib/components/content_component.dart
+++
@@ -1,6 +0,0 @@
-import 'package:angular2/core.dart';
-
-@Component(
-    selector: 'app-content',
-    templateUrl: 'templates/content_component.html')
-class ContentComponent {}
