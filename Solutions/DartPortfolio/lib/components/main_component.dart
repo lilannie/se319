00ec542f2310a11a02f5014e878faec214c053eb
Figgynewts <znewton@iastate.edu>
2016-11-11 18:29:20 -0600

    forms and auth

--- Solutions/DartPortfolio/lib/components/main_component.dart
+++ Solutions/DartPortfolio/lib/components/main_component.dart
@@ -4,8 +4,20 @@ import 'package:dart_portfolio/components/pnm_form_component.dart';
 import 'package:dart_portfolio/components/active_form_component.dart';
 import 'package:dart_portfolio/components/login_form_component.dart';
 
+import 'package:dart_portfolio/services/auth_service.dart';
+
 @Component(
     selector: 'app-main',
     templateUrl: 'templates/main_component.html',
-    directives: const[PnmFormComponent,ActiveFormComponent, LoginFormComponent])
-class MainComponent {}
+    directives: const[PnmFormComponent,ActiveFormComponent, LoginFormComponent],
+    providers: const[AuthService])
+class MainComponent implements OnInit{
+
+  final AuthService _authService;
+  MainComponent(this._authService);
+  bool authorized = false;
+
+  void ngOnInit(){
+    authorized = _authService.getAuth();
+  }
+}
