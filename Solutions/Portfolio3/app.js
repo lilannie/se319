72358b5046f3d092fa3822920ef5a7d5bce750d7
Figgynewts <znewton@iastate.edu>
2016-12-09 14:08:34 -0600

    Finished HTML Parser

--- Solutions/Portfolio3/app.js
+++ Solutions/Portfolio3/app.js
@@ -5,15 +5,31 @@ document.addEventListener('DOMContentLoaded', function(e){
 
 	document.getElementById('code-submit').addEventListener('click', function(e){
 
-		parser = eval(parserSource);
-
-		displayResult(document.getElementById('code-input').value, document.getElementById('language-select').value, parser)
+		displayResult(document.getElementById('code-input').value, document.getElementById('language-select').value)
 	});
 });
 
-function displayResult(input,language, parser) {
-	console.log(input);
-	document.getElementById('result').innerHTML = parse(input, parser);
+function displayResult(input,language) {
+	var parser = null;
+	switch(language) {
+		case "js":
+			parser=null;
+			break;
+		case "css":
+			parser=null;
+			break;
+		case "html":
+			parser = htmlParser;
+			break;
+		case "php":
+			parser = null;
+			break;
+	}
+
+	if(parser == null) return;
+	document.getElementById('result-pretty').className = language;
+	document.getElementById('result-pretty').innerHTML = parse(input, parser);
+	document.getElementById('result-raw').innerHTML = parse(document.getElementById('result-pretty').innerHTML, htmlParser);
 }
 
 function parse(input, parser) {
--- Solutions/Portfolio3/index.php
+++ Solutions/Portfolio3/index.php
@@ -9,8 +9,7 @@
 	<meta name="keywords" content="tool, code, color" />
 	<meta name="author" content="Zach Newton" />
 	<link rel="stylesheet" href="stylesheet.css">
-	<script type="text/javascript" src="lib/peg-0.10.0.js"></script>
-	<script type="text/javascript" src="parser.js"></script>
+	<script type="text/javascript" src="parsers/htmlParser.js"></script>
 	<script type="text/javascript" src="app.js"></script>
 </head>
 <body>
@@ -19,23 +18,26 @@
 	<h2>Code Colorizer</h2>
 
 	<div>
-	Use this tool to colorize your code by adding span tags to the input. Span tages will include classes
+	Use this tool to colorize your code by adding span tags to the input. Span tags will include classes
 	that can be used for coloring.
 	</div>
 
 	<form>
 		<label for="language-select">Language:</label>
 		<select id="language-select" name="language">
-			<option selected>JS</option>
-			<option>CSS</option>
-			<option>HTML</option>
+			<option value="js">JS</option>
+			<option value="css">CSS</option>
+			<option value="html" selected>HTML</option>
+			<option value="php">PHP</option>
 		</select>
 		<label for="code-input">Code:</label>
-		<textarea id="code-input" name="code" placeholder="Paste your code in here..." rows="10">1+1</textarea>
+		<textarea id="code-input" name="code" placeholder="Paste your code in here..." rows="10"></textarea>
 		<button type="button" id="code-submit">Colorize Code</button>
 
-		<label>Result:</label>
-		<pre id="result"></pre>
+		<label>Result (pretty):</label>
+		<pre id="result-pretty"></pre>
+		<label>Result (raw):</label>
+		<pre id="result-raw"></pre>
 	</form>
 </main>
 
