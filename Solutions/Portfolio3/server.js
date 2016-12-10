3fd732fb98f476138936e24c037d5c8e70f003c0
Annie Steenson <anniesteenso@gmail.com>
2016-12-10 00:19:53 -0600

    Server now serves 'index.php' at localhost:8081

--- Solutions/Portfolio3/server.js
+++ Solutions/Portfolio3/server.js
@@ -1,14 +1,25 @@
 var http = require("http");
+var fs = require('fs');
 
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8081);
+fs.readFile('./index.php', function(err, html) {
+	if (err) {
+		response.writeHead(404);
+      	response.write("Not Found!");
+		throw err;
+	}
+
+	http.createServer(function (request, response) {
+	   // Send the HTTP header 
+	   // HTTP Status: 200 : OK
+	   // Content Type: text/plain
+	   response.writeHead(200, {'Content-Type': 'html'});
+	   
+	   response.write(html);
+	   // Send the response body as "Hello World"
+	   response.end();
+	}).listen(8081);
+});
+
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
