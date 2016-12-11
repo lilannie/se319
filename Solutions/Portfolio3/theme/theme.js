b49bf8f66047dfe2fb5ab26f401d93467fef2371
Figgynewts <znewton@iastate.edu>
2016-12-10 20:43:32 -0600

    Finished Theme Picker page functionality

--- Solutions/Portfolio3/theme/theme.js
+++ Solutions/Portfolio3/theme/theme.js
@@ -7,22 +7,64 @@ document.addEventListener('DOMContentLoaded', function (e){
 	document.getElementById('CSS-style-printed').innerHTML = parse(document.getElementById('CSS-style').innerHTML,cssParser);
 	document.getElementById('JSON-style-printed').innerHTML = parse(document.getElementById('JSON-style').innerHTML,cssParser);
 
-	var html = "#324D5C";
-	var html_tag = "#46B29D";
-	var html_attr = "#E37B40";
-	var html_attr_value = "#DE5B49";
-	var html_comment = "#aaa";
+	var html_color_inputs = document.getElementsByClassName('html-color-input');
+	for (var i = 0; i < html_color_inputs.length; i++)
+	{
+		html_color_inputs[i].addEventListener('change', function(e){
+			var html = "#"+document.getElementById('html-main-color').value;
+			var html_tag ="#"+ document.getElementById('html-tag-color').value;
+			var html_attr = "#"+document.getElementById('html-attr-color').value;
+			var html_attr_value = "#"+document.getElementById('html-attr-value-color').value;
+			var html_comment = "#"+document.getElementById('html-comment-color').value;
+			changeHTMLStyle(html, html_tag, html_attr, html_attr_value, html_comment);
+		});
+	}
+	var css_color_inputs = document.getElementsByClassName('css-color-input');
+	for (i = 0; i < css_color_inputs.length; i++)
+	{
+		css_color_inputs[i].addEventListener('change', function(e){
+			var css = "#"+document.getElementById('css-main-color').value;
+			var css_tag_selector = "#"+document.getElementById('css-selector-tag-color').value;
+			var css_class_selector = "#"+document.getElementById('css-selector-class-color').value;
+			var css_id_selector = "#"+document.getElementById('css-selector-id-color').value;
+			var css_attr = "#"+document.getElementById('css-attr-color').value;
+			var css_comment = "#"+document.getElementById('css-comment-color').value;
+			changeCSSStyle(css, css_tag_selector, css_class_selector, css_id_selector, css_attr, css_comment);
+		});
+	}
+	var json_color_inputs = document.getElementsByClassName('json-color-input');
+	for (i = 0; i < json_color_inputs.length; i++)
+	{
+		json_color_inputs[i].addEventListener('change', function(e){
+			var json = "#"+document.getElementById('json-main-color').value;
+			var json_key = "#"+document.getElementById('json-key-color').value;
+			var json_string = "#"+document.getElementById('json-string-color').value;
+			var json_number = "#"+document.getElementById('json-number-color').value;
+			changeJSONStyle(json, json_key, json_string, json_number);
+		});
+	}
+
+
+	var html = "#"+document.getElementById('html-main-color').value;
+	var html_tag ="#"+ document.getElementById('html-tag-color').value;
+	var html_attr = "#"+document.getElementById('html-attr-color').value;
+	var html_attr_value = "#"+document.getElementById('html-attr-value-color').value;
+	var html_comment = "#"+document.getElementById('html-comment-color').value;
 	changeHTMLStyle(html, html_tag, html_attr, html_attr_value, html_comment);
 
-	var css = "#324D5C";
-	var css_tag_selector = "#DE5B49";
-	var css_class_selector = "#E37B40";
-	var css_id_selector = "#F0CA4D";
-	var css_attr = "#46B29D";
-	var css_comment = "#aaa";
+	var css = "#"+document.getElementById('css-main-color').value;
+	var css_tag_selector = "#"+document.getElementById('css-selector-tag-color').value;
+	var css_class_selector = "#"+document.getElementById('css-selector-class-color').value;
+	var css_id_selector = "#"+document.getElementById('css-selector-id-color').value;
+	var css_attr = "#"+document.getElementById('css-attr-color').value;
+	var css_comment = "#"+document.getElementById('css-comment-color').value;
 	changeCSSStyle(css, css_tag_selector, css_class_selector, css_id_selector, css_attr, css_comment);
-//TODO
-	changeJSONStyle(json, key, string, number);
+
+	var json = "#"+document.getElementById('json-main-color').value;
+	var json_key = "#"+document.getElementById('json-key-color').value;
+	var json_string = "#"+document.getElementById('json-string-color').value;
+	var json_number = "#"+document.getElementById('json-number-color').value;
+	changeJSONStyle(json, json_key, json_string, json_number);
 });
 
 
@@ -101,7 +143,7 @@ function changeJSONStyle(json, key, string, number){
 "}"+"\n"+
 ".json-attribute,"+"\n"+
 ".json-array > .json-array-value{"+"\n"+
-"	padding-left: 2em;"+"\n"+
+"	padding-left: 1em;"+"\n"+
 "	display: list-item;"+"\n"+
 "}"+"\n"+
 "";
