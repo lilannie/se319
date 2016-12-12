976efd11092a84690e0eb551d03fd991a1a49a6c
Annie Steenson <anniesteenso@gmail.com>
2016-12-11 19:50:45 -0600

    working on backend

--- Solutions/Portfolio3/server.js
+++ Solutions/Portfolio3/server.js
@@ -2,14 +2,45 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var firebase = require('firebase');
+var router = express.Router();
 
-app.use(express.static('./public'));
-app.use(bodyParser.urlencoded({ extended: false }));
+var db = firebase.initializeApp({
+    apiKey: "AIzaSyDtQ_NKVgypWNhreYxl36a0yRMazntsq6c",
+    authDomain: "myrecruiter-9d526.firebaseapp.com",
+    databaseURL: "https://myrecruiter-9d526.firebaseio.com",
+    storageBucket: "myrecruiter-9d526.appspot.com",
+    messagingSenderId: "416574837620"
+ });
+var auth = db.auth();
+
+//app.use(express.static('./public'));
+//app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
-   res.send("./" + "index.html");
-})
+	var user = auth.currentUser;
 
+	if (user) {
+		console.log("User");
+		console.log(user);
+   		res.send("./" + "index.html");
+	} else {
+		console.log("Not logged in");
+		res.redirect("/login");
+	}
+})
+app.get('/login', function(req, res) {
+	var user = auth.currentUser;
+
+	if (user) {
+		res.redirect("/");
+	} else {
+		console.log("Not logged in");
+	}
+})
+app.post('/login', function(req, res) {
+	console.log(req);
+	res.redirect("/");
+})
 app.get('/theme', function(req, res) {
 	var options = {
     	root: __dirname + '/public/',
@@ -17,9 +48,6 @@ app.get('/theme', function(req, res) {
 	res.sendFile("theme.html");
 })
 
-var server = app.listen(8081, function () {
-   var host = server.address().address;
-   var port = server.address().port;
-   
-   console.log("Example app listening at http://%s:%s", host, port);
+var server = app.listen(8080, function () {
+   console.log("Listening at http://localhost:8080");
 })
